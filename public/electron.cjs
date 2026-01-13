5b6fc3e95133d2bb7a3987d788f40cbb5e56865f
Jose Candia <josecandiap@gmail.com>
2026-01-12 22:10:46 -0300

    Initial commit

---
+++ public/electron.cjs
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: "Pixel Pomodoro",
+        width: 400,
+        height: 600,
+    });
+
+    const startUrl = url.format({
+        pathname: path.join(__dirname, '../dist/index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+
+    mainWindow.loadURL(startUrl);
+}
+
+app.whenReady().then(createMainWindow);
